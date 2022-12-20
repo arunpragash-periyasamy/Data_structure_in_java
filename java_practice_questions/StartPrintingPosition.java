48196e233374b8c4439e984f302ff75876dc6cc3
arunpragash-annanperiasamy <arunpragash46@gmail.com>
2022-12-20 20:49:03 +0530

    java program to print from the given index and print the remaining elements

--- java_practice_questions/StartPrintingPosition.java
+++ java_practice_questions/StartPrintingPosition.java
@@ -2,7 +2,32 @@ import java.util.Scanner;
 public class StartPrintingPosition{
     public static void main(String[] args){
         Scanner read = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
         int size = read.nextInt();
-        
+        int[] array = new int[size]; 
+        System.out.println("\nEnter the Array Elements: ");
+        for(int i=0;i<size;i++){
+            array[i] = read.nextInt();
+        }
+        System.out.print("Enter the index to print: ");
+        System.out.println("");
+        int index = read.nextInt();
+        for(int i=index-1; i<size;i++){
+            System.out.print(array[i]+" ");
+        }
+        for(int i = 0; i< index-1;i++){
+            System.out.print(array[i]+" ");
+        }
     }
 }
+
+
+/*
+Sample input 
+5
+10 20 30 40 50
+4
+40 50 10 20 30
+
+executed using normal array but need to implement using linked list
+ */
